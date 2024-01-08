47002446b64b51850bb1f50e4566f2328679195b
Elsa Menegaux <elsa.menegaux@free.fr>
2024-01-08 11:07:35 +0100

    :)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel ="stylesheet" href="boardstyle.css">
+    <link rel ="stylesheet" href="css/boardstyle.css">
     <title>Puissance 4</title>
 </head>
 <body>
@@ -11,8 +11,10 @@
         <table class="table">
         <?php 
             for ($i=0; $i<6; $i++) {
+                $iindex= $i ;
                 echo "<tr>";
                 for ($j=0; $j<7;$j++){
+                    $jindex= $j ;
                     echo "<td></td>";
                 };
                 echo "</tr>";
