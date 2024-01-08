0c344c9d6dbcb14339f2929c32102b06f8ee9431
Elsa Menegaux <elsa.menegaux@free.fr>
2024-01-08 11:37:00 +0100

    on annule en fait

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
             for ($i=0; $i<6; $i++) {
                 echo "<tr>";
                 for ($j=0; $j<7;$j++){
-                    echo "<td class='index ".$i."/".$j."'></td>";
+                    echo "<td></td>";
                 };
                 echo "</tr>";
             };
