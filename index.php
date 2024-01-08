fb7220557d95f0ca07971cac5bcdb7b73d788b8c
Elsa Menegaux <elsa.menegaux@free.fr>
2024-01-08 10:26:26 +0100

    empty grid

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href="style.css">
+    <title>Puissance 4</title>
+</head>
+<body>
+    <div class="board">
+        <table class="table">
+        <?php 
+            for ($i=0; $i<6; $i++) {
+                echo "<tr>";
+                for ($j=0; $j<7;$j++){
+                    echo "<td></td>";
+                };
+                echo "</tr>";
+            };
+        ?>
+        </table>
+    </div>
+</body>
+</html>
